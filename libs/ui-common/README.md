342e5e6f0785f807a3bc13e22156253af35ef8b0
Moppedijk <social@oppedijk.online>
2023-12-16 14:42:50 +0100

    Rename to ui-common

--- libs/ui-common/README.md
+++ libs/ui-common/README.md
@@ -1,7 +1,7 @@
-# ui-components
+# ui-common
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test ui-components` to execute the unit tests.
+Run `nx test ui-common` to execute the unit tests.
--- libs/ui-common/jest.config.ts
+++ libs/ui-common/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'ui-components',
+  displayName: 'ui-common',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/ui-components',
+  coverageDirectory: '../../coverage/libs/ui-common',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
