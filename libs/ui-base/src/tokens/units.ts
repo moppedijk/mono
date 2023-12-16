de4c15f4c8dfb79b581b9dbcb02c33caed88acc2
Moppedijk <social@oppedijk.online>
2023-12-16 14:36:00 +0100

    Added ui-base and ui-layout

---
+++ libs/ui-base/src/tokens/units.ts
@@ -0,0 +1,3 @@
+export const unitSmall = '.8rem';
+export const unitDefault = '1.6rem';
+export const unitLarge = '2.4rem';
---
+++ libs/ui-layout/src/lib/ui-layout/ui-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mo-ui-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui-layout.component.html',
+  styleUrl: './ui-layout.component.scss',
+})
+export class UiLayoutComponent {}
