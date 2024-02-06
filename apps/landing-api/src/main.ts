10e40c350a49d1d37c11727a4f34c0ac4fbfa5bf
Moppedijk <social@oppedijk.online>
2024-02-06 08:20:04 +0100

    Changed environment variables for api

--- apps/landing-api/src/main.ts
+++ apps/landing-api/src/main.ts
@@ -2,32 +2,23 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   const port = process.env.PORT || 3000;
-  // TODO: Put host in environment
-  const host = process.env.PORT ? 'https://moppedijk-api.onrender.com' : 'http://localhost';
+  const host = process.env.API_HOST;
+  const origin = process.env.API_ORIGIN;
 
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://moppedijk.nl',
-    ]
-  });
+  app.enableCors({ origin });
 
   await app.listen(port);
   
-  Logger.log(
-  `🚀 Application is running on: ${host}:${port}/${globalPrefix}`,
-  );
+  Logger.log(`🚀 Application is running on: ${host}:${port}/${globalPrefix}`);
 }
 
 bootstrap();
